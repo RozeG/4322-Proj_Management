d70106fee60a65bdcbe9bd3e8c834f4ccec8c8a6
Rosa G <rosa.gandara@mavs.uta.edu>
2016-10-24 09:01:58 -0500

    SecondCommit

---
+++ app/src/main/java/com/funinc/amusementparklinesaver/activity/ReservePage.java
@@ -0,0 +1,13 @@
+package com.funinc.amusementparklinesaver.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReservePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserve_page);
+    }
+}
